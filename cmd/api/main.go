c42c37b9eb7c43cc883645c187f6b9078af95a4e
Thiago Luis <thiago.luis88@gmail.com>
2025-01-14 23:27:52 -0300

    fix: main

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,121 +1,25 @@
 package main
 
 import (
-	"archive/zip"
-	"encoding/json"
-	"fmt"
-	"io"
-	"os"
-	"os/exec"
-	"path/filepath"
-
-	ffmpeg "github.com/u2takey/ffmpeg-go"
+	"github.com/aws/aws-sdk-go-v2/service/sqs/types"
+	"github.com/thiagoluis88git/hack-video-processing/pkg/environment"
+	"github.com/thiagoluis88git/hack-video-processing/pkg/queue"
 )
 
-type FFProbeOutput struct {
-	Stream []struct {
-		NBFrames string `json:"nb_frames"`
-	} `json:"streams"`
-}
+func main() {
+	chnMessages := make(chan *types.Message)
 
-func mainFFMPEG() {
-	videoFile := "video.mp4" // Path to your video file
-	outputDir := "output"    // Directory to store extracted images
-	zipFile := "images.zip"  // Name of the zip file
+	env := environment.LoadEnvironmentVariables()
 
-	// Create the output directory if it doesn't exist
-	if err := os.MkdirAll(outputDir, os.ModePerm); err != nil {
-		fmt.Println("Error creating output directory:", err)
-		return
-	}
+	queueManager := queue.ConfigQueueManager(env)
 
-	// Extract frames from the video
-	err := extractFrames(videoFile, outputDir)
-	if err != nil {
-		fmt.Println("Error extracting frames:", err)
-		return
-	}
+	go queueManager.PollMessages(chnMessages)
 
-	maxFrames, err := getFrameCount(videoFile)
-
-	if err != nil {
-		fmt.Println("Error getting max frame count:", err)
-		return
-	}
-
-	fmt.Printf("Max quantity: %v\n", maxFrames)
-
-	// Create a ZIP file with the extracted images
-	err = zipFiles(outputDir, zipFile)
-	if err != nil {
-		fmt.Println("Error creating zip file:", err)
-		return
-	}
-
-	fmt.Println("Images extracted and zipped successfully.")
-}
-
-// Function to extract frames using ffmpeg-go
-func extractFrames(videoFile string, outputDir string) error {
-	// Use ffmpeg-go to extract frames
-	return ffmpeg.Input(videoFile).
-		Output(filepath.Join(outputDir, "image%d.jpg"), ffmpeg.KwArgs{"vf": "fps=1"}).
-		Run()
-}
-
-// Function to zip files in a directory
-func zipFiles(sourceDir string, zipFile string) error {
-	zipFileHandle, err := os.Create(zipFile)
-	if err != nil {
-		return err
-	}
-	defer zipFileHandle.Close()
-
-	zipWriter := zip.NewWriter(zipFileHandle)
-	defer zipWriter.Close()
-
-	err = filepath.Walk(sourceDir, func(file string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-		if info.IsDir() {
-			return nil
+	for message := range chnMessages {
+		if message == nil {
+			return
 		}
 
-		zipEntry, err := zipWriter.Create(info.Name())
-		if err != nil {
-			return err
-		}
-
-		fileHandle, err := os.Open(file)
-		if err != nil {
-			return err
-		}
-		defer fileHandle.Close()
-
-		_, err = io.Copy(zipEntry, fileHandle)
-		return err
-	})
-
-	return err
-}
-
-func getFrameCount(videoFile string) (string, error) {
-	cmd := exec.Command("ffprobe", "-v", "error", "-select_streams", "v:0", "-show_entries", "stream=nb_frames", "-of", "json", videoFile)
-
-	output, err := cmd.Output()
-	if err != nil {
-		return "", err
+		queueManager.WriteMessage(message)
 	}
-
-	var ffProbeOutput FFProbeOutput
-	if err := json.Unmarshal(output, &ffProbeOutput); err != nil {
-		return "", err
-	}
-
-	if len(ffProbeOutput.Stream) > 0 {
-		return ffProbeOutput.Stream[0].NBFrames, nil
-	}
-
-	return "0", nil // If no frames found
 }
